12429fb513408043bb8bba814924a538ea63933e
Jesse B. Miller <jesse@jessebmiller.com>
2019-04-06 10:18:01 -0500

    [WIP] few runners

--- sdk/js/index.js
+++ sdk/js/index.js
@@ -1,4 +1,30 @@
-const runners = {}
+const runners = {
+  "linux-bash-game-v0": (agent, formatAddress, gameData) => {
+    // presume we are running a node linux bash squad client
+    // start a process identified in the game data
+    
+  },
+  "web-game-v0": (agent, formatAddress, gameData) => {
+    if (!window) {
+      console.log("web-game-v0 needs to launch from web squad")
+      return
+    }
+    // web-game-v0 requires a url for where the game is
+    // TODO how do we document (or self document) the game data
+    // requirements for the different game types
+    // Consider adding macros for formatAddress, agent.id, agent addr
+    // etc. This would allow us to create a large number of options
+    // for passing context to the web game
+    let gameURL = new URL(gameData.__webGameV0.gameURL)
+    gameURL.searchParams.append("squad-web-game-v0-format", formatAddress)
+    gameURL.searchParams.append("squad-web-game-v0-agent-nick", agent.id)
+    gameURL.searchParams.append(
+      "squad-web-game-v0-agent-addr",
+      agent.agentId,
+    )
+    window.open(gameURL, "_blank")
+  }
+}
 
 const registerRunner = (type_, runner) => {
   runners[type_] = runner
@@ -12,6 +38,12 @@ const runGame = (agent, gameAddress, formatAddress) => {
     throw result
   }
   const runner = runners[result.Ok.Game.type_]
+
+  // Runners unpack the format and present it to the game in a consistent way
+  // should this be done with the game data as well? what if the game data is
+  // very large?
+  // - no, game contributors have the ability to factor address game
+  // data however they want but they don't have that ability with formats
   return runner(agent, formatAddress, result.Ok.Game.data)
 }
 
