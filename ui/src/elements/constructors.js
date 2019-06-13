f70a17599f3cf9c4e32c74f7398cd27d54c5e878
Jesse B. Miller <jesse@jessebmiller.com>
2019-06-12 20:41:51 -0500

    wip

---
+++ ui/src/elements/constructors.js
@@ -0,0 +1,16 @@
+/**
+ * Constructor functions for the element types to be decerialized
+ * correctly by the zome
+ */
+
+export function Game(name, platform, data) {
+  return {Game: {name, platform, data}}
+}
+
+export function Format(name, components) {
+  return {Format: {name, components}}
+}
+
+export function Component(name, type, data) {
+  return {Component: {name, type, data}}
+}
---
+++ ui/src/indexes/actions.js
@@ -0,0 +1,7 @@
+/**
+ * Indexes are lists of elements stored as holochain entries with
+ * elements linked to them.
+ */
+
+const GET_INDEX
+
