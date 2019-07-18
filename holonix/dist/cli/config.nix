714abcbe14ca4cb45ef198fde189fd86ad2eee63
Ezra Weller <ezra.weller@gmail.com>
2019-07-18 07:13:58 -0400

    git ignore

---
+++ holonix/dist/cli/config.nix
@@ -0,0 +1,10 @@
+{
+ name = "cli";
+
+ sha256 = {
+  linux = "1n1s0brvgjhc4m4xxrg1p0y9n5j3c9yb4mc8xz9f69hdrhmpiy1b";
+  darwin = "18cbhn3qd3phmypapzxhm0cp8hs64z6yq7yiy8kvah4xwf42mwa2";
+ };
+
+ binary = "hc";
+}
