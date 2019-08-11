9fd7668391358a252e166ccbb14be21d798e8667
Ezra Weller <ezra.weller@gmail.com>
2019-08-11 17:27:36 -0400

    testing AutoBond

--- curation/contracts/AutoBond.sol
+++ curation/contracts/AutoBond.sol
@@ -1,8 +1,9 @@
-pragma solidity >=0.4.21 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract AutoBond {
+  using SafeMath for uint256;
 
   constructor () public {}
 
@@ -16,28 +17,28 @@ contract AutoBond {
   mapping(bytes32 => Bond) public bonds;
 
   // newBond makes a new bond and buys the first set of units
-  function newBond(address curve, bytes32 bondId, uint256 units) payable public {
+  function newBond(address curve, bytes32 bondId, uint256 units) public payable {
     require(curve != address(0), "Must specify a curve address");
-    require(bonds[bondId].address == address(0), "Bond id exists");
-    Bond bond;
-    bond.curve = curve;
+    require(bonds[bondId].curve == address(0), "Bond id exists");
+    Bond memory bond = Bond({ curve: curve, supply: 0 });
+    // bond.curve = curve;
     bonds[bondId] = bond;
     if (units > 0) {
-      _buy(units, bondId, msg.sender, msg.vaue);
+      _buy(units, bondId, msg.sender, msg.value);
     }
   }
 
   function _buy(
     uint256 units,
     bytes32 bondId,
-    address purchaser,
+    address payable purchaser,
     uint256 value
   ) internal {
-    require(uints > 0, "cannot buy zero or less units");
-    Bond b = bonds[bondId];
+    require(units > 0, "cannot buy zero or less units");
+    Bond storage b = bonds[bondId];
     uint256 price = Curve(b.curve).buyPrice(b.supply, units);
     require(price <= value, "must send enough to cover price");
-    uint256 refund = value.min(price);
+    uint256 refund = value.sub(price);
     b.supply = b.supply.add(units);
     b.balances[purchaser] = b.balances[purchaser].add(units);
     purchaser.transfer(refund);
@@ -52,7 +53,7 @@ contract AutoBond {
     // TODO users will want some way to control whether they want to hold
     // depending on price
     require(units > 0, "cannot sell zero or fewer units");
-    Bond b = bonds[bondId];
+    Bond storage b = bonds[bondId];
     require(units >= b.balances[msg.sender], "Must own enough units to sell");
     uint256 price = Curve(b.curve).sellPrice(b.supply, units);
     b.supply = b.supply.sub(units);
@@ -62,8 +63,8 @@ contract AutoBond {
 }
 
 contract Curve {
-  function buyPrice(uint256 supply, uint256 units) public returns (uint256);
-  function sellPrice(uint256 supply, uint256 units) public returns (uint256);
+  function buyPrice(uint256 supply, uint256 units) public view returns (uint256);
+  function sellPrice(uint256 supply, uint256 units) public view returns (uint256);
 }
 
 contract SimpleLinearCurve is Curve {
@@ -71,13 +72,13 @@ contract SimpleLinearCurve is Curve {
 
   constructor () public {}
 
-  function buyPrice (uint256 supply, uint256 units) public returns (uint256) {
+  function buyPrice (uint256 supply, uint256 units) public view returns (uint256) {
     // sum of the series from units to units + supply
     // units * ((supply + units) / 2)
-    return units.mul(supply.add(units).div(2))
+    return units.mul(supply.add(units).div(2));
   }
 
-  function sellPrice (uint256 supply, uint256 units) public returns (uint256) {
-    return buyPrice(supply.sub(units), units)
+  function sellPrice (uint256 supply, uint256 units) public view returns (uint256) {
+    return buyPrice(supply.sub(units), units);
   }
 }
--- curation/test/AutoBond.js
+++ curation/test/AutoBond.js
@@ -2,14 +2,45 @@ const AutoBond = artifacts.require("AutoBond")
 const SimpleLinearCurve = artifacts.require("SimpleLinearCurve")
 
 contract("AutoBond", ([alice, bob, ...accounts]) => {
+  let curve
+  let autoBond
 
+  async function simpleCurvePrice(supply, units) {
+    return await curve.buyPrice(supply, units)
+  }
+
+  beforeEach(async () => {
+    autoBond = await AutoBond.new()
+    curve = await SimpleLinearCurve.new()
+  })
+
+  it("should know true is true", () => {
+    assert.equal(true, true)
+  })
+  
   // alice can create bond A
+  it("creates functioning SimplerLinear bonds", async () => {
+    const bondId1 = "0x12ad4"
+    const bondId2 = "0xfffff"
+    await autoBond.newBond(curve.address, bondId1, 0, {from: alice})
+    try {
+      await autoBond.buy(200, bondId1, {from: bob, value: await simpleCurvePrice(0, 199)})
+      assert.fail("Failed: Bought 200 from bond A for the price of 199.")
+    } catch(e) {}
+    await autoBond.buy(200, bondId1, {from: bob, value: await simpleCurvePrice(0, 200)})
+    await autoBond.newBond(curve.address, bondId2, 100, {from: bob, value: await simpleCurvePrice(0, 100)})
+    try {
+      await autoBond.buy(200, bondId2, {from: alice, value: await simpleCurvePrice(100, 199)})
+      assert.fail("Failed: Bought 200 from bond B for the price of 199.")
+    } catch(e) {}
+    await autoBond.buy(200, bondId2, {from: alice, value: await simpleCurvePrice(100, 200)})
+  })
 
   // bob can buy the first 2 units of bond A for 3 wei
 
   // bob can create bond B buy the first 2 units for 3 wei
 
-  // alice can buy the next 2 units of bond B for 9 wei
+  // alice can buy the next 2 units of bond B for 7 wei
 
   // bob can sell a unit of bond B for 4 wei
 
