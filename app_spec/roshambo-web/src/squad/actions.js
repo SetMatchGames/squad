1798723ed2c2b08cc40c1949a5070446430fd77c
Jesse B. Miller <jesse@jessebmiller.com>
2019-08-23 23:53:55 -0500

    checkpoint

--- app_spec/roshambo-web/src/squad/actions.js
+++ app_spec/roshambo-web/src/squad/actions.js
@@ -1,6 +1,10 @@
 import { metastore } from '../sdk/js'
 import { getUrlParams } from '../utils'
 
+// TODO: move discoverGameOpponents to squad-sdk
+// implementing it here now to avoid merge conflicts
+//import { discoverGameOpponents } from '../sdk/js'
+
 // Connecting to squad actions
 export const STORE_SQUAD_URI = "STORE_SQUAD_URI"
 export const CONNECTING_TO_SQUAD = "CONNECTING_TO_SQUAD"
@@ -146,7 +150,7 @@ export function formatComponentsFailure(error) {
 async function getAllComponents(addresses) {
   let definitions = []
   for (let i in addresses) {
-    let definition = { 
+    let definition = {
       definition: await metastore.getDefinition(addresses[i]),
       key: addresses[i]
     }
@@ -154,3 +158,62 @@ async function getAllComponents(addresses) {
   }
   return definitions
 }
+
+// getting opponents
+export const LISTEN_FOR_GAME_OPPONENTS = "LISTEN_FOR_GAME_OPPONENTS"
+export const FOUND_GAME_OPPONENT = "FOUND_GAME_OPPONENT"
+export const GAME_OPPONENTS_FAILURE = "GAME_OPPONENTS_FAILURE"
+
+
+export const SELECT_GAME_OPPONENT = "SELECT_GAME_OPPONENT"
+export const GAME_OPPONENT_CONFIRMED = "GAME_OPPONENT_CONFIRMED"
+export const GAME_OPPONENT_DENIED = "GAME_OPPONENT_DENIED"
+export const GAME_OPPONENT_SELECT_FAILURE = "GAME_OPPONENT_SELECT_FAILURE"
+
+export function selectOpponent(opponent) {
+  return {type: SELECT_GAME_OPPONENT, opponent}
+}
+
+export function getGameOpponents(game) {
+  return (dispatch) => {
+    dispatch(listenForGameOpponents(game))
+    discoverGameOpponents(game, (opponent) => {
+      foundGameOpponent(opponent)
+    })
+  }
+}
+
+export function listenForGameOpponents(game) {
+  return {type: LISTEN_FOR_GAME_OPPONENTS, game}
+}
+
+export function foundGameOpponent(opponent) {
+  return {type: FOUND_GAME_OPPONENT, opponent}
+}
+
+export function gameOpponentsFailure(error) {
+  return {type: GAME_OPPONENTS_FAILURE, error}
+}
+
+/**
+ *  discover game opponents functionality will move to squad SDK
+ */
+
+const IPFS = require('ipfs')
+
+function gameChannelKey(game) {
+  return `squad.games/${game.key}`
+}
+
+export async function discoverGameOpponents(game, cb) {
+  const node = await IPFS.create({EXPERIMENTAL: {pubsub: true}})
+  const chan = gameChannelKey(game)
+  node.pubsub.subscribe(
+    chan,
+    (msg) => { console.log(`message on ${chan}:`, msg) },
+    {discover: true}
+  )
+  node.pubsub.publish(gameChannelKey(game), Buffer.from("message", 'utf8'))
+  console.log(node)
+}
+
