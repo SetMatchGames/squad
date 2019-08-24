a16d581a552b9bb75f22a33d858d65f945dc6e20
Jesse B Miller <jesse@jessebmiller.com>
2019-08-24 09:52:01 -0500

    Package the SDK (#18)
    SDK packaging
    
    * initial curation commit
    * testing AutoBond
    * initial auto bond tests work
    * testing updates
    * starting curation api
    * checkpoint
    * curation-api works
    * ignore curation/build, package sdk

--- sdk/js/metastore-api/index.js
+++ sdk/js/metastore-api/index.js
@@ -6,16 +6,17 @@ function webSocketConnection(uri) {
   squad.connection = new WebSocket(uri)
   return squad.connection
 }
-  
+
 function mockConnection(mock) {
   squad.connection = mock
   return squad.connection
 }
-  
+
 function on(message, f) {
+  console.warn("let's depreciate the on function")
   squad.connection.on(message, f)
 }
-  
+
 async function call(zome, method, inputs) {
   console.log("squad.call", zome, method, inputs)
 
@@ -37,11 +38,11 @@ async function call(zome, method, inputs) {
   }
   return result.Ok
 }
-  
+
 async function createDefinition(definition) {
   return await call("definitions", "create_definition", {definition})
 }
-  
+
 async function getDefinition(address) {
   return await call("definitions", "get_definition", {address})
 }
@@ -51,15 +52,27 @@ async function getAddress(entry) {
 }
 
 async function getCatalogAddresses(catalog_type, catalog_name) {
-  return await call("definitions", "get_catalog_links", {catalog_type, catalog_name})
+  return await call(
+    "definitions",
+    "get_catalog_links",
+    {catalog_type, catalog_name}
+  )
 }
-  
+
 async function getAllDefinitionsOfType(catalog_type) {
-  return await call("definitions", "get_all_definitions_of_type", {catalog_type})
+  return await call(
+    "definitions",
+    "get_all_definitions_of_type",
+    {catalog_type}
+  )
 }
-  
+
 async function getDefinitionsFromCatalog(catalog_type, catalog_name) {
-  return await call("definitions", "get_definitions_from_catalog", {catalog_type, catalog_name})
+  return await call(
+    "definitions",
+    "get_definitions_from_catalog",
+    {catalog_type, catalog_name}
+  )
 }
 
 function close() {
