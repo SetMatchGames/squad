5a2e456d768e1aacee0a52de5f744650639847a6
Jesse B. Miller <jesse@jessebmiller.com>
2019-09-09 18:34:11 -0500

    ipfs pubsub networking shim

--- ui/src/index.js
+++ ui/src/index.js
@@ -8,7 +8,11 @@ import { Provider } from 'react-redux'
 import store from './store'
 
 import { connectToSquad } from './squad/actions'
-import { fetchCatalog, switchDefinitionForm } from './definitions/actions'
+import {
+  fetchCatalog,
+  switchDefinitionForm,
+  shareDefinitions,
+} from './definitions/actions'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -24,6 +28,7 @@ store.dispatch(connectToSquad(
     dispatch(fetchCatalog("Component", "Component Catalog"))
     dispatch(fetchCatalog("Format", "Format Catalog"))
     dispatch(switchDefinitionForm("Game", "Game Catalog"))
+    shareDefinitions()
   },
   (error, dispatch) => {
     console.warn(error)
