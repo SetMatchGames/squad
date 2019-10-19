61c77e6db57dabd4e5c865d20a40de0970d5ba95
Jesse B. Miller <jesse@jessebmiller.com>
2019-10-19 15:58:11 -0500

    squad injects configurable endpoint url into launched web games

--- app_spec/roshambo-web/src/utils.js
+++ app_spec/roshambo-web/src/utils.js
@@ -1,23 +1,3 @@
-export function getUrlParams(paramNames) {
-  let url = window.location.href
-  if(!url.includes("?")) { 
-      throw new Error("No params provided: squadUri param required to connect to squad.")
-  }
-  let urlSegments = url.split("?")
-  let paramSegment = urlSegments[urlSegments.length - 1]
-  let paramArray = paramSegment.split(/[&=]+/)
-  const params = {}
-  for (let p in paramNames) {
-    let paramName = paramNames[p]
-    if(paramArray.indexOf(paramName) === -1) { 
-      throw new Error(`${paramName} param not provided in URL params.`)
-    }
-    let param = paramArray[paramArray.indexOf(paramName)+1]
-    params[paramName] = param
-  }
-  return params
-}
-
 export function addActionToState(action, state) {
   action.status = action.type
   let newState = Object.assign(
--- sdk/js/README.md
+++ sdk/js/README.md
@@ -4,3 +4,12 @@
 squad = require('squad')
 squad.webSocketConnection("localhost:8888")
 ```
+
+# Configuration
+
+two config files are currently created by build processes
+`./curation-config.json` is created by the curation market deploy process and
+stores the ethereum contract addresses.
+
+`./squad-config.json` is created by the "build process" (`make develop` right
+now) and it holds the `sdkUrl`. This is passed to games by game runners.
