1ee4763593b904a52d11b72c4ff4f6c977ee64ec
Ezra Weller <ezra.weller@gmail.com>
2019-11-24 10:17:20 -0500

    start of auto-chess

---
+++ packages/app-spec-web-new/automatic-chess/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Board from './Board.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Automatic Chess</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
