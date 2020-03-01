b37945245657b8881f4a995507162ad85dad6b74
Ezra Weller <ezra.weller@gmail.com>
2020-03-01 17:07:55 -0500

    swarm!

--- packages/app-spec-squad-chess/src/index.js
+++ packages/app-spec-squad-chess/src/index.js
@@ -2,6 +2,7 @@ import m from 'mithril'
 import { metastore } from '@squad/sdk'
 import chess from './rules.js'
 import settings from './settings.json'
+import Peer from 'peerjs'
 
 import state from './state.js'
 import Board from './Board.js'
@@ -37,10 +38,53 @@ async function init () {
 
     state.loadedFormat = Object.assign(JSON.parse(formatToLoad.data), { pieces })
     console.log('loaded format', state.loadedFormat)
+    const seedId = `${settings.gameAddress}${urlParams.get('format')}-0`
+    console.log('seedId', seedId)
+    const peer = new Peer('Ezra12345678')
+    peer.connect('Jesse12345678')
+    
+    // const peer = validPeer(seedId)
+    // connectToAnyPeer(seedId, peer)
+
     state.game = chess.createGame(state.loadedFormat)
   }
 
   return 'Squad Chess initialized'
+}
+
+function validPeer(id) {
+  let validPeer = false
+  let peer
+  while (!validPeer) {
+    console.log(id, nextPeerId(id))
+    id = nextPeerId(id)
+    peer = new Peer(id, {
+      host: 'localhost',
+      port: 9000,
+      path: 'peer-discovery'
+    })
+    validPeer = !peer.destroyed
+  }
+  return peer
+}
+
+function connectToAnyPeer(id, peer) {
+  let connected = 0
+  while (connected < 10) {
+    console.log('connect', id, nextPeerId(id))
+    id = nextPeerId(id)
+    peer.connect(id)
+    console.log(peer)
+    connected += 1
+  }
+}
+
+function nextPeerId(id) {
+  console.log('previous id', id)
+  let [seedId, n] = id.split('-')
+  n = parseInt(n) + 1
+  console.log('n', n)
+  return `${seedId}-${n}`
 }
 
 metastore.webSocketConnection(settings.metastoreWs)
---
+++ packages/p2p/index.js
@@ -0,0 +1,80 @@
+const swarm = require('discovery-swarm')
+const getPort = require('get-port')
+const crypto = require('crypto')
+
+let peers = {}
+let matchedPeer = false
+const id = crypto.randomBytes(32)
+let sw
+
+async function listen() {
+  sw = swarm({ id })
+  const port = await getPort()
+  sw.listen(port)
+  console.log(`Listening on port ${port}`)
+}
+
+function joinChannel(channel, opts = {}) {
+  sw.join(channel)
+  sw.on('connection', (connection, info) => {
+    const peerId = info.id.toString('hex')
+    peers[peerId] = connection
+    console.log(`First connection in channel ${channel} established: ${peerId}`)
+    if (opts.matchFirst) {
+      matchById(peerId, channel)
+      matchedPeer = peerId
+      connection.on('data', data => {
+        console.log(String(data))
+      })
+    }
+  })
+}
+
+function matchById(targetId, oldChannel) {
+  sw.leave(oldChannel)
+  peers = {}
+  const hash = crypto.createHash('sha1')
+  const newChannel = hash
+    .update(consistentStringSplice(id.toString('hex'), targetId))
+    .digest('hex')
+  joinChannel(newChannel)
+}
+
+async function firstAvailableMatch(channel) {
+  await listen()
+  joinChannel(channel, { matchFirst: true })
+}
+
+function write(string) {
+  if (matchedPeer) peers[matchedPeer].write(string)
+}
+
+function disconnect() {
+  sw = null
+}
+
+function consistentStringSplice(s1, s2) {
+  const orderedStrings = alphabetize(s1, s2, 0)
+  return orderedStrings[0]+orderedStrings[1]
+}
+
+function alphabetize(s1, s2, i) {
+  if (s1[i] > s2[i]) {
+    return [s1, s2]
+  } else if (s2[i] > s1[i]) {
+    return [s2, s1]
+  } else {
+    return alphabetize(s1, s2, i+1)
+  }
+}
+
+module.exports = {
+  peers,
+  matchedPeer,
+  firstAvailableMatch,
+  write
+}
+
+firstAvailableMatch('cheese').then()
+setInterval(() => write('hello'), 1000)
+
