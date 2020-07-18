8e09df07bcb2b21a97f8f7fb4802a75517e587b9
Ezra Weller <ezra.weller@gmail.com>
2020-07-18 19:53:39 -0400

    persistent buy and sell buttons (#122)

--- packages/app-spec-squad-chess/src/FormatSelector.js
+++ packages/app-spec-squad-chess/src/FormatSelector.js
@@ -1,8 +1,9 @@
-/* global URL */
+/* global URL, location */
 
 import m from 'mithril'
 import state from './state.js'
 import BuyDefinitionButton from './BuyDefinitionButton.js'
+import SellDefinitionButton from './SellDefinitionButton.js'
 import { shortHash } from './utils.js'
 
 const FormatSelector = {
@@ -14,25 +15,54 @@ const FormatSelector = {
       '#format-selector',
       m('h3', 'Available Formats'),
       orderedFormats.map(address => {
-        const name = `${state.squad.rawFormats[address].name} (${shortHash(address)})`
-        if (state.owned[address]) {
-          const url = new URL(window.location)
-          url.search = `?format=${address}`
-          return m(
-            'div',
-            m(`a[href=${url}]`, name),
-            ` – Market size: ${state.marketCaps[address]}`
-          )
-        } else {
-          return m(
-            'div',
-            name,
-            m(BuyDefinitionButton, { bondId: address }),
-            ` – Market size: ${state.marketCaps[address]}`
-          )
-        }
+        return m(FormatCard, { key: address })
       })
     )
+  }
+}
+
+const FormatCard = {
+  view: (vnode) => {
+    const address = vnode.key
+    const name = `${state.squad.rawFormats[address].name} (${shortHash(address)})`
+    return m(
+      'p',
+      name,
+      ` – Market size: ${state.marketCaps[address]}`,
+      m(LoadFormatButton, { bondId: address }),
+      m(BuyDefinitionButton, { bondId: address }),
+      m(SellDefinitionButton, { bondId: address })
+    )
+  }
+}
+
+const LoadFormatButton = {
+  view: (vnode) => {
+    const bondId = vnode.attrs.bondId
+    let owned = false
+    let num = 0
+    if (state.owned[bondId] > 0) {
+      owned = true
+      num = state.owned[bondId]
+    }
+    return m(
+      'form.load-format',
+      m(
+        'button',
+        { onclick: handleLinkFactory(bondId), disabled: !owned },
+        'Load'
+      ),
+      `(Owned: ${num})`
+    )
+  }
+}
+
+const handleLinkFactory = (bondId) => {
+  return (e) => {
+    e.preventDefault()
+    const url = new URL(window.location)
+    url.search = `?format=${bondId}`
+    location.href = url
   }
 }
 
